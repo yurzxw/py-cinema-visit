e625de21dadb10aacf8a225c1df74a85d3fbc0f6
Yura <yuradziavoruk5@gmail.com>
2026-09-01 18:21:48 +0300

    sol

---
+++ app/cinema/bar.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from app.people.customer import Customer
+
+
+class CinemaBar:
+    @staticmethod
+    def sell_product(product: str, customer: Customer) -> None:
+        print(f"Cinema bar sold {product} to {customer.name}.")
